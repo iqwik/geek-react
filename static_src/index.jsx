0a8d7a1180518f5498a5aef22e7c7bee10afe4dd
artem <artem@debian>
2019-04-27 00:33:40 +0300

    На каждое отправленное сообщение должен отвечать "робот" (используйте componentDidUpdate)

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class Component extends React.Component {
-    render(){
-        return (
-            [
-                <h1 key='h1'>Hello World! Just one more Test here</h1>,
-                <p key='desc'>Simple description here</p>
-            ]
-        )
-    }
-}
+import App from './components/App';
 
 ReactDOM.render(
-    <Component />,
+    <App />,
     document.getElementById('root'),
 );
